5c5c7a042952a3b0107c4d7eceb065414a550980
kowalanumero1 <srramsurrun@gmail.com>
2025-04-08 18:06:56 +0400

    initial commit

---
+++ word matching/main.py
@@ -0,0 +1,13 @@
+def match_words(words):
+    ctr = 0
+    lst = []
+    for word in words:
+        if len(word) > 1 and word[0] == word[-1]
+            ctr +=1
+            lst.append(word)
+
+    print("list of words with the first and last character that are same\n", lst)
+    return ctr
+
+count = match_words(['abc', 'cfc', 'aba', '1221'])
+print("number of words that have simillar first and last characters:" count)
